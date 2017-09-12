b18cc729cea72429b19d3bc71adfceae07edd2ca
cgrooves <caleb.j.groves@gmail.com>
2017-09-12 12:06:27 -0600

    Duplicate

--- scrimmage_code/scrimmage_code/other_functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef OTHER_FUNCTIONS_H
-#define OTHER_FUNCTIONS_H
-
-// Puts all appropriate pins to a low state and puts
-// the arduino in as low a power consumption state as
-// can be.
-void power_down();
-
-#endif
